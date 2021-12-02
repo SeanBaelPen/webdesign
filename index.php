94756b40babd331f8c723b9df3958a8b2f1c857b
DianoEdriel <dianoedrieljohn.ed@gmail.com>
2021-12-02 12:49:58 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,293 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <title>Louis</title>
+</head>
+
+<body>
+  <!--<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">MENU<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="#">SHOP<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="#">ABOUT US<span class="sr-only">(current)</span></a>
+        </li>
+        <a class="navbar-brand" href="#"><img src="Pictures/logo.png" alt=""></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <li class="nav-item active">
+          <a class="nav-link" href="#">LOCATIONS<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="#">ACCOUNT<span class="sr-only">(current)</span></a>
+        </li>
+      </ul>
+    </div>
+  </nav>-->
+
+  <?php
+    include 'include/navbar_default.html';
+  
+  ?>
+
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active" style="margin-right: 40px;"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2" style="margin-left: 40px;"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="d-block w-100" src="https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="First slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="https://images.pexels.com/photos/324028/pexels-photo-324028.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Second slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="https://images.pexels.com/photos/683039/pexels-photo-683039.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Third slide">
+      </div>
+    </div>
+  </div>
+
+  <div class="productCarousel">
+    <p>Our Products</p>
+  </div>
+
+  <table>
+    <tr>
+      <td>
+        <a href="#">
+          <div style="height:100%;width:100%">
+            Coffee
+          </div>
+        </a>
+      </td>
+      <td>
+        <a href="#">
+          <div style="height:100%;width:100%">
+            Baked Goods
+          </div>
+        </a>
+      </td>
+      <td>
+        <a href="#">
+          <div style="height:100%;width:100%">
+            Coffee Grounds
+          </div>
+        </a>
+      </td>
+      <td>
+        <a href="#">
+          <div style="height:100%;width:100%">
+            Equipment
+          </div>
+        </a>
+      </td>
+    </tr>
+  </table>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-12">
+        <div id="inam" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <div class="container">
+                <div class="row">
+                  <div class="col-sm-12 col-lg-4">
+                    <div class="card" style="width: 300px;margin: auto;">
+                      <img src="https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="card-img-top">
+                      <div class="card-body">
+                        <h4 class="card-title">Why you should use skin masks ?</h4>
+                        <p class="card-text">Skin masks help us to make are skin fresh and also they protect our skin from the harm rays of sun</p>
+                        <button type="button" class="btn btn-warning">Read More</button>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-sm-12 col-lg-4">
+                    <div class="card" style="width: 300px;">
+                      <img src="https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="card-img-top">
+                      <div class="card-body">
+                        <h4 class="card-title">Why you should use skin masks ?</h4>
+                        <p class="card-text">Skin masks help us to make are skin fresh and also they protect our skin from the harm rays of sun</p>
+                        <button type="button" class="btn btn-warning">Read More</button>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-sm-12 col-lg-4">
+                    <div class="card" style="width: 300px;">
+                      <img src="https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="card-img-top">
+                      <div class="card-body">
+                        <h4 class="card-title">Why you should use skin masks ?</h4>
+                        <p class="card-text">Skin masks help us to make are skin fresh and also they protect our skin from the harm rays of sun</p>
+                        <button type="button" class="btn btn-warning">Read More</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <div class="container">
+                <div class="row">
+                  <div class="col-sm-12 col-lg-4">
+                    <div class="card" style="width: 300px;margin: auto;">
+                      <img src="https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="card-img-top">
+                      <div class="card-body">
+                        <h4 class="card-title">Why you should use skin masks ?</h4>
+                        <p class="card-text">Skin masks help us to make are skin fresh and also they protect our skin from the harm rays of sun</p>
+                        <button type="button" class="btn btn-warning">Read More</button>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-sm-12 col-lg-4">
+                    <div class="card" style="width: 300px;">
+                      <img src="https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="card-img-top">
+                      <div class="card-body">
+                        <h4 class="card-title">Why you should use skin masks ?</h4>
+                        <p class="card-text">Skin masks help us to make are skin fresh and also they protect our skin from the harm rays of sun</p>
+                        <button type="button" class="btn btn-warning">Read More</button>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-sm-12 col-lg-4">
+                    <div class="card" style="width: 300px;">
+                      <img src="https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="card-img-top">
+                      <div class="card-body">
+                        <h4 class="card-title">Why you should use skin masks ?</h4>
+                        <p class="card-text">Skin masks help us to make are skin fresh and also they protect our skin from the harm rays of sun</p>
+                        <button type="button" class="btn btn-warning">Read More</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <a href="#inam" class="carousel-control-prev" data-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+          </a>
+          <a href="#inam" class="carousel-control-next" data-slide="next">
+            <span class="carousel-control-next-icon"></span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="productCarousel">
+    <p>Lorem Ipsum</p>
+  </div>
+
+  <div class="grid-container">
+    <div class="grid-item1">
+      <p class="titleForInfos">Our story</p>
+      <p class="tinyDetails">Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br>
+        Quisque ligula tortor, aliquam a euismod vitae, lacinia eu <br>
+        purus. Suspendisse a
+      </p>
+      <div class"moreButton">
+        <button type="button" class="btn btn-outline-dark btn-lg">Learn More</button>
+      </div>
+    </div>
+
+    <div class="grid-item2">
+      <img id="firstImage" src="https://images.pexels.com/photos/4790059/pexels-photo-4790059.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+    </div>
+  </div>
+
+  <div class="grid-container">
+    <div class="grid-item-one">
+      <img id="secondImage" src="outdoorshop.jpg" alt="">
+    </div>
+
+    <div class="grid-item-two">
+      <p class="titleForInfos">Our Locations</p>
+      <p class="tinyDetails">Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br>
+        Quisque ligula tortor, aliquam a euismod vitae, lacinia eu <br>
+        purus. Suspendisse a
+      </p>
+      <div class"moreButton">
+        <button type="button" class="btn btn-outline-dark btn-lg">Learn More</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="grid-container">
+    <div class="grid-item1">
+      <p class="titleForInfos">Become a Member</p>
+      <p class="tinyDetails">Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br>
+        Quisque ligula tortor, aliquam a euismod vitae, lacinia eu <br>
+        purus. Suspendisse a
+      </p>
+      <button type="button" class="btn btn-outline-dark btn-lg">Learn More</button>
+    </div>
+
+    <div class="grid-item2">
+      <img id="firstImage" src="https://images.pexels.com/photos/4790059/pexels-photo-4790059.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+    </div>
+  </div>
+
+  <div class="footer">
+    <div class="socialMedia">
+      <img src="https://cdn3.iconfinder.com/data/icons/capsocial-round/500/facebook-512.png" alt="">
+      <img src="https://cdn.icon-icons.com/icons2/1109/PNG/512/1486053611-twitter_79195.png" alt="">
+      <img src="https://cdn3.iconfinder.com/data/icons/popular-services-brands/512/instagram-512.png" alt="">
+    </div>
+
+    <div class="footerFlex">
+    <div class="box-1">
+        <img src="logo.png" alt="logo.png" height="100px" class="footerLogo">
+    </div>
+    <div class="box-2">
+      <span>About</span>
+      <div class="aboutInfos">
+        <span>FAQ</span>
+        <span>Locations</span>
+        <span>Our Story</span>
+        <span>Join Us</span>
+      </div>
+    </div>
+      <div class="box-3">
+        <span>Shop Online</span>
+        <div class="shopInfos">
+          <span>Menu</span>
+          <span>Equpiment</span>
+        </div>
+      </div>
+      <div class="box-4">
+        <span>Contacts</span>
+        <div class="contactsInfo">
+          <span>info@louis.com.ph</span>
+          <span>801-####</span>
+        </div>
+    </div>
+</div>
+
+
+      <div class="newsletter">
+        <p>Subscribe to our newsletter</p>
+        <input type="text" placeholder="Enter Email" name="email" required>
+      </div>
+
+    </div>
+
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
+</html>
