54010c425388fb1b2444698dc8a05ba0b30a0825
DianoEdriel <dianoedrieljohn.ed@gmail.com>
2021-12-03 21:46:46 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,14 +10,12 @@
   <link rel="stylesheet" href="css/main.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <title>Louis</title>
-</head>
-
-<body>
-  
   <?php
     include 'include/navbar_default.html';
   ?>
- 
+</head>
+
+<body>
  <div class="container-fluid">
   <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
@@ -219,14 +217,16 @@
     </div>
   </div>
 
-  <?php
-    include 'include/footer.html';
-  
-  ?>
+ 
 
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
+<footer>
+<?php
+    include 'include/footer.html';
+  ?>
+</footer>
 
 </html>
